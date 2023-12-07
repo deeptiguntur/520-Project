48c40c0e076fb2234ce1296bac078e62592f078d
Anamika Ghosh <anamikaghosh990@gmail.com>
2023-12-06 23:40:37 -0500

    small changes in order page

--- Frontend/shopfinity/src/app/app-routing.module.ts
+++ Frontend/shopfinity/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AddProductComponent } from './seller-page/add-product/add-product.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignUpComponent},
-  {path: 'add-product', component: AddProductComponent},
+  {path: 'seller/add-product', component: AddProductComponent},
+  {path:'user/order-details',component:OrderDetailsComponent}
 ];
 
 @NgModule({
