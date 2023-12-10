03512693f21cf1325682ab73d60501a9168c6f35
vybhavisri <natluri@umass.edu>
2023-12-10 14:18:39 -0500

    Updating product view page

--- Frontend/shopfinity/src/app/app-routing.module.ts
+++ Frontend/shopfinity/src/app/app-routing.module.ts
@@ -4,17 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AddProductComponent } from './seller-page/add-product/add-product.component';
 import { SellerPageComponent } from './seller-page/seller-page.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { OrderDetailsComponent } from './order-details/order-details.component';
-
+import { ProductViewComponent } from './product-view/product-view.component';
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'add-product', component: AddProductComponent},
   {path: 'seller/dashboard', component: SellerPageComponent},
-  {path: 'user/cart', component: OrderDetailsComponent},
-  {path: 'user/product-list', component: ProductListComponent},
+  {path: 'product/view', component: ProductViewComponent}
+
 ];
 
 @NgModule({
